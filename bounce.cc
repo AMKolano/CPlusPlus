671ed96122f66ef7a773e28deaaf790a61907ae8
Anna Kolano <anna.maria.kolano@cern.ch>
2017-03-27 11:39:58 +0200

    Add draw() and move() functions

--- bounce.cc
+++ bounce.cc
@@ -1,28 +1,46 @@
 #include <iostream>
 
-int main() {
 
-  char particleSymbol = 'x';
-  double particlePosition = 0;
-  double particleSpeed = 6.3;
-  int maxColumn = 80;
-  int minColumn = 0;
-  int timeStep = 0;
-  int stopTime = 60;
+char particleSymbol = 'x';
+double particlePosition = 0;
+double particleSpeed = 6.3;
+int maxColumn = 80;
+int minColumn = 0;
+int timeStep = 0;
+int stopTime = 60;
 
-  while (timeStep < stopTime) {
+
+void draw(){
     for (int i = minColumn; i < particlePosition; i++) {
-      std::cout << " ";
+         std::cout << " ";
     }
     std::cout << particleSymbol << std::endl;
-    particlePosition += particleSpeed;
-    if (particlePosition >= maxColumn) {
+   
+}
+
+
+
+void move(){
+ particlePosition += particleSpeed;
+
+
+   if (particlePosition >= maxColumn) {
       particlePosition = maxColumn;
       particleSpeed = -particleSpeed;
     } else if (particlePosition < minColumn) {
       particlePosition = minColumn;
       particleSpeed = -particleSpeed;
-    }    
-    timeStep++;
+    }
+
+
+}
+
+int main() {
+
+  while (timeStep < stopTime) {
+        draw();
+        move();    
+        timeStep++;
   }
 }
+
