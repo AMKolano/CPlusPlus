3ccf40a440f75739235f8ee682106b5beac07430
Anna Kolano <anna.maria.kolano@cern.ch>
2017-03-28 11:31:21 +0200

    Use struct for creating a particle with its parameters

--- bounce.cc
+++ bounce.cc
@@ -10,12 +10,20 @@ void move(double&, double&);
 void printScreen(const int, char[]);
 void clearScreen(const int, char[]);
 
+//one array of particle struct
+
+struct Particle {
+    char Symbol;
+    double Speed;
+    double Position;
+};
+
 
 int main() {
     const int npart = 3;
-    double Speed[npart] = {5, 6.3, 2};
-    double Position[npart] = {0, 1, 5};
-    char Symbol[npart] = {'x', 'O' ,'v'};
+    
+    Particle ps[npart] = {{'x', 5.6, 0 },{'O', 6.3, 2},{'V',4, 1 } };// make array ps of particles with symbol, speed and position, order kept as in struct
+
     int timeStep = 0;
     const int stopTime = 60;
     const int screenSize =  maxColumn - minColumn + 1; 
@@ -27,8 +35,8 @@ int main() {
     while (timeStep < stopTime) { 
         clearScreen(screenSize, screen);// put blank spaces before each entry
         for (int i=0; i < npart; i++) {
-        draw(Position[i], Symbol[i], screen);//add arguments
-        move(Position[i], Speed[i]);
+        draw(ps[i].Position, ps[i].Symbol, screen);//add arguments
+        move(ps[i].Position, ps[i].Speed); //ps.Position[i], could not work coz ps is a pointer but struct is not, but if i access one of the particles ps, i can access stuff in there, hence ps[i].Position works, i acess particle nr i, then .Position acess its position
         }
         printScreen(screenSize, screen);
         timeStep++;
