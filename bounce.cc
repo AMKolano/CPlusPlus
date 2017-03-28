07736390c65484efc24e5ecb4cce0b12d3b9abec
Anna Kolano <anna.maria.kolano@cern.ch>
2017-03-28 15:19:51 +0200

    Add functions to the struct, removing them from the main body

--- bounce.cc
+++ bounce.cc
@@ -4,48 +4,57 @@ const int maxColumn = 80;
 const int minColumn = 0;
 
 //using sdt::cout; using std::endl;
+
+//make a class'ish
 struct Particle {
     char Symbol;
     double Speed;
     double Position;
+
+    void draw(char screen[]){ // Particle * ps is done aoutomatically and changed to "this" by the compiler
+       screen[static_cast<int>(this->Position)] = this->Symbol;
+    }
+
+    void move(){
+        this->Position += this->Speed;
+
+        if (this->Position >= maxColumn) {
+        this->Position = maxColumn;
+        this->Speed = -this->Speed;
+        } else if (this->Position < minColumn) {
+        this->Position = minColumn;
+        this->Speed = -this->Speed;
+        }
+    }
+    void initialize(char Symbol, double Speed, double Position ){
+        this->Symbol = Symbol;
+        this->Speed = Speed;
+        this->Position = Position;
+    }
 };
-void initialize(Particle* , char , double, double);
-void draw(const Particle*, char[]);
-void move(Particle*);
+
 void printScreen(const int, char[]);
 void clearScreen(const int, char[]);
-
-
-/*
-struct Particle {
-    char Symbol;
-    double Speed;
-    double Position;
-};  
-*/
 
 int main() {
     const int npart = 3;
     
     Particle ps[npart];
-    initialize(&ps[0],'V',4, 1 );
-    initialize(&ps[1],'X',3, 1 );
-    initialize(&ps[2],'O',4.6, 2 );
+    ps[0].initialize('V',4, 1 );
+    ps[1].initialize('X',3, 1 );
+    ps[2].initialize('O',4.6, 2 );
 
     int timeStep = 0;
     const int stopTime = 60;
     const int screenSize =  maxColumn - minColumn + 1; 
     char* screen = new char[screenSize]; // dynamically allocate the memory
 
-
- 
-
     while (timeStep < stopTime) { 
         clearScreen(screenSize, screen);// put blank spaces before each entry
         for (int i=0; i < npart; i++) {
-
-        draw(&ps[i], screen);//add arguments
-        move(&ps[i]); 
+        //equivalent to the one below &ps[i]->draw(screen):   
+        ps[i].draw(screen);//magic happens and no longer need arguments here?
+        ps[i].move(); 
         }
         printScreen(screenSize, screen);
         timeStep++;
@@ -53,20 +62,6 @@ int main() {
     delete [] screen; // delete memory used
 }
 
-void initialize(Particle* ps, char Symbol, double Speed, double Position ){
-    ps->Symbol = Symbol;
-    ps->Speed = Speed;
-    ps->Position = Position;
-  
- 
-
-
-}
-
-
-void draw(const Particle* ps, char screen[]){ //ref & here saves memory, but cant draw const particles now, const Particle does not allow to change it
-     screen[static_cast<int>(ps->Position)] = ps->Symbol; 
-}
 
 void printScreen(const int screenSize, char* screen){ //equivalent to below 
     for (int i =0; i < screenSize; i++)
@@ -76,22 +71,21 @@ void printScreen(const int screenSize, char* screen){ //equivalent to below
 
 void clearScreen(const int screenSize, char screen[]){ 
     for (int i = minColumn; i < screenSize; i++)
-        screen[i] = ' '; //dont clear the buffer here yet
-   
+        screen[i] = ' '; //dont clear the buffer here yet 
 }
 
-void move(Particle* ps){
- ps->Position += ps->Speed;
 
-   if (ps->Position >= maxColumn) {
-      ps->Position = maxColumn;
-      ps->Speed = -ps->Speed;
-    } else if (ps->Position < minColumn) {
-     ps->Position = minColumn;
-      ps->Speed = -ps->Speed;
-    }
 
-}
+
+
+
+
+
+
+
+
+
+
 
 
 
