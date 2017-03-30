8eecda29f8f804bc6c8364b9a3f727b2b4cb08f3
Anna Kolano <anna.maria.kolano@cern.ch>
2017-03-30 16:11:34 +0200

    Std vector use is awesome

--- bounce.cc
+++ bounce.cc
@@ -4,7 +4,7 @@
 #include "Screen.hh"
 #include <fstream>
 #include <string>
-
+#include <vector>
 
 const int Size = (maxColumn - minColumn + 1);
 //using sdt::cout; using std::endl;
@@ -24,17 +24,18 @@ int main() {
     in >> Np;
  
   
-    const size_t  npart = Np; 
-    Particle* ps = new Particle[npart];
- // now dynamically assigned array, before Particle ps = [npart]
+    const size_t  npart = Np;
+
+    std::vector<Particle> ps;//vector of type Particle  
+   // Particle* ps = new Particle[npart]; // now dynamically assigned array, before Particle ps = [npart]
     
     for (size_t i=0; i < npart; ++i){
 
        char S;
        double v, pos;
        in >> S >> v >> pos;
-       ps[i] = Particle(S,v, pos);
-     std::cout<<i <<std::endl;
+       ps.push_back(Particle(S,v, pos));
+       std::cout<<i <<std::endl;
      }
     in.close();
 
