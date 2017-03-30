4dfe638a20a51e8fe15269174ecdb8b0553a4fec
Anna Kolano <anna.maria.kolano@cern.ch>
2017-03-30 13:13:14 +0200

    Screen class, functions outside of the class

--- bounce.cc
+++ bounce.cc
@@ -9,24 +9,44 @@ const int Size = (maxColumn - minColumn + 1);
 class  Screen{
 public:
 
-    void printScreen(){
+    void printScreen();
+
+    void clearScreen();
+       
+    Screen(const Screen& origin);
+    
+    Screen(const size_t screenSize);
+  
+    Screen& operator = (const Screen& rhs); 
+   ~Screen();
+   char& operator[](const unsigned);
+
+private: // good practice put privite at the bottom
+    size_t screenSize; //fixed screen for now
+    char * buffer;
+
+};
+
+/////////outside of screen///////////////
+
+void Screen::printScreen(){
         for (size_t i =0; i < screenSize; i++)
             std::cout << this->buffer[i]; //dont clear the buffer here yet
             std::cout << std::endl; //clear buffer
-    }
+}
 
-    void clearScreen(){
-        for (size_t i = minColumn; i < screenSize; i++)
-            buffer[i] = ' '; //dont clear the buffer here yet 
-    }
-       
-    Screen(const Screen& origin) : screenSize(origin.screenSize), buffer(new char[this->screenSize]) {
-           std::copy(origin.buffer, origin.buffer+screenSize, this->buffer);
-           }
-    
-    Screen(const size_t screenSize) : screenSize(screenSize), buffer(new char[screenSize]) {} 
-  
-     Screen& operator = (const Screen& rhs) {
+void Screen::clearScreen(){
+    for (size_t i = minColumn; i < screenSize; i++)
+        buffer[i] = ' '; //dont clear the buffer here yet 
+}
+
+Screen::Screen(const Screen& origin) : screenSize(origin.screenSize), buffer(new char[this->screenSize]) {
+        std::copy(origin.buffer, origin.buffer+screenSize, this->buffer);
+        }
+
+Screen::Screen(const size_t screenSize) : screenSize(screenSize), buffer(new char[screenSize]) {}
+
+Screen& Screen::operator = (const Screen& rhs) {
     if (this != &rhs) {
       this->screenSize = rhs.screenSize;
       delete [] this->buffer;
@@ -34,40 +54,21 @@ public:
       std::copy(rhs.buffer, rhs.buffer+rhs.screenSize, this->buffer);
       }
     return *this;
+}
 
-   }
-  
-/*
-   //swap function
-   Screen& operator = (Screen rhs){
-     this->swap(rhs);
-     return *this;
-    }
+Screen::~Screen(){
+    delete [] this->buffer;
+}
 
-   void swap(Screen& other){
-    std::swap(this->screenSize, other.screenSize);//standard library swap
-    std::swap(this->buffer, other.buffer);
-    }
-
- */
-   ~Screen(){
-        delete [] this->buffer;
-     }
- 
-   
-
-   char& operator[](const unsigned Position){
-        return this->buffer[Position];
-   }
-
- 
-private: // good practice put privite at the bottom
-    size_t screenSize; //fixed screen for now
-    char * buffer;
-
-};
+char& Screen::operator[](const unsigned Position){
+    return this->buffer[Position];
+}
 
 
+
+
+
+///////////////Particle/////////////////
 class  Particle {
 public:   
     void draw(Screen& screen) const{ 
